faa42df9930711b8a4cecea912ee049ca3d4ca6f
David (DongYun)  Kim <pander0616@hotmail.com>
2023-10-29 12:07:28 -0600

    initial commit

---
+++ exercise.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Exercise Page</title>
+    <link rel="stylesheet" href="./menuStyle.css">
+
+    <div class="container">
+        <div class="menu-toggle">
+            <input type="checkbox" id="menu-check">
+            <div class="menu-btn">
+                <label for="menu-check">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </label>
+            </div>
+            <ul class="menu">
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Services</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+
+
+
+</html>
